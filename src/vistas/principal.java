a8bee9e1d0ce537ac08da731688880a7908f7a46
fernando sombra <fersombra10@gmail.com>
2022-07-09 20:18:44 -0300

    arreglo del menu

--- src/vistas/principal.java
+++ src/vistas/principal.java
@@ -10,12 +10,14 @@ package vistas;
  * @author fer10
  */
 public class principal extends javax.swing.JFrame {
-      
 
-    
+    /**
+     * Creates new form principal
+     */
     public principal() {
         initComponents();
         this.setExtendedState(MAXIMIZED_BOTH);
+        
     }
 
     /**
@@ -27,55 +29,78 @@ public class principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDesktopPane1 = new javax.swing.JDesktopPane();
+        VentanaPrincipal = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
+        Menu = new javax.swing.JMenu();
+        Ayuda = new javax.swing.JMenuItem();
+        Salir = new javax.swing.JMenuItem();
+        Ventas = new javax.swing.JMenu();
+        ventas = new javax.swing.JMenuItem();
+        Mantenimiento = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
-        jMenu4 = new javax.swing.JMenu();
-        jMenu3 = new javax.swing.JMenu();
+        Reportes = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 688, Short.MAX_VALUE)
+        javax.swing.GroupLayout VentanaPrincipalLayout = new javax.swing.GroupLayout(VentanaPrincipal);
+        VentanaPrincipal.setLayout(VentanaPrincipalLayout);
+        VentanaPrincipalLayout.setHorizontalGroup(
+            VentanaPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 657, Short.MAX_VALUE)
         );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 337, Short.MAX_VALUE)
+        VentanaPrincipalLayout.setVerticalGroup(
+            VentanaPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 444, Short.MAX_VALUE)
         );
 
-        jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu.jpg"))); // NOI18N
-        jMenu1.setText("MENU");
+        Menu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu.jpg"))); // NOI18N
+        Menu.setText("MENU");
 
-        jMenuItem1.setText("Ayuda");
-        jMenu1.add(jMenuItem1);
+        Ayuda.setText("Ayuda");
+        Menu.add(Ayuda);
 
-        jMenuItem2.setText("Salir");
-        jMenu1.add(jMenuItem2);
+        Salir.setText("Salir");
+        Salir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SalirActionPerformed(evt);
+            }
+        });
+        Menu.add(Salir);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(Menu);
 
-        jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/ventas.png"))); // NOI18N
-        jMenu2.setText("VENTAS");
+        Ventas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/ventas.png"))); // NOI18N
+        Ventas.setText("VENTAS");
 
-        jMenuItem3.setText("Generar Ventas");
-        jMenu2.add(jMenuItem3);
+        ventas.setText("Generar Venta");
+        Ventas.add(ventas);
 
-        jMenuBar1.add(jMenu2);
+        jMenuBar1.add(Ventas);
 
-        jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/reportes.png"))); // NOI18N
-        jMenu4.setText("REPORTES");
-        jMenuBar1.add(jMenu4);
+        Mantenimiento.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/mantenimiento.png"))); // NOI18N
+        Mantenimiento.setText("MANTENIMIENTO");
 
-        jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/mantenimiento.png"))); // NOI18N
-        jMenu3.setText("MANTENIMIENTO");
-        jMenuBar1.add(jMenu3);
+        jMenuItem1.setText("Cliente");
+        Mantenimiento.add(jMenuItem1);
+
+        jMenuItem2.setText("Producto");
+        Mantenimiento.add(jMenuItem2);
+
+        jMenuItem3.setText("Vendedor");
+        Mantenimiento.add(jMenuItem3);
+
+        jMenuBar1.add(Mantenimiento);
+
+        Reportes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/reportes.png"))); // NOI18N
+        Reportes.setText("REPORTES");
+
+        jMenuItem4.setText("Reporte Ventas");
+        Reportes.add(jMenuItem4);
+
+        jMenuBar1.add(Reportes);
 
         setJMenuBar(jMenuBar1);
 
@@ -83,15 +108,22 @@ public class principal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(VentanaPrincipal)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(VentanaPrincipal)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_SalirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -129,14 +161,18 @@ public class principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane jDesktopPane1;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuItem Ayuda;
+    private javax.swing.JMenu Mantenimiento;
+    private javax.swing.JMenu Menu;
+    private javax.swing.JMenu Reportes;
+    private javax.swing.JMenuItem Salir;
+    private javax.swing.JDesktopPane VentanaPrincipal;
+    private javax.swing.JMenu Ventas;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem ventas;
     // End of variables declaration//GEN-END:variables
 }
